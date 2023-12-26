220d3415ba334b04ea8ebd2f3b73400ebfe13345
Furkan Boncuk <furkanboncuk2000@gmail.com>
2023-12-27 00:01:24 +0300

    react-router-dom kütüphanesi eklendi ve sayfa yönlendirmeleri için layout oluşturuldu.

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Homepage from "../src/pages/homepage/Homepage"
+import BookDetail from "../src/pages/bookDetail/BookDetail"
+import ShoppingCart from "../src/pages/shoppingCart/ShoppingCart"
+import { useState } from 'react';
+import { CartProvider } from './context/CartContext';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* PROVIDER */}
+      <CartProvider>
+        <Navbar onSearch={handleSearch} />
+        <Routes>
+          <Route path="/" element={<Homepage searchTerm={searchTerm} />} />
+          <Route path="/detail/:id" element={<BookDetail />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+        </Routes>
+        <Footer />
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
