bc77fc5bd0ca06474325348792c0f4490619a71c
Furkan Boncuk <furkanboncuk2000@gmail.com>
2023-12-29 01:31:15 +0300

    CartContext kullanılarak ShoppingCart sayfasında ürünlerin adedi ve ürünlerin toplam tutarı gibi bilgiler sağlanmıştır. Ayrıca tasarım düzenlenmiştir.

--- book-store/src/pages/shoppingCart/ShoppingCart.jsx
+++ book-store/src/pages/shoppingCart/ShoppingCart.jsx
@@ -1,44 +1,68 @@
+import { Link } from "react-router-dom"
 import ShoppingCartItem from "../../components/shoppingCartItem/ShoppingCartItem"
-import {useCart} from "../../context/CartContext"
+import { useCart } from "../../context/CartContext"
 import styles from "./shoppingCart.module.css"
+import { useEffect } from "react"
 
 const ShoppingCart = () => {
-  const {cartState} = useCart()
-  const {items} = cartState
+  const { cartState, getTotalPrice } = useCart()
+  const { items, totalPrice, totalItems } = cartState
 
-  console.log(items)
-  
+  useEffect(() => {
+    getTotalPrice()
+  },[cartState?.items])
+
   return (
     <div className={styles.shoppingCartContainer}>
       <div className={styles.cartTitle}>
         Your Cart
       </div>
 
-      <div className={styles.orderContainer}>
-        <div className={styles.itemList}>
-          {
-            items && 
-            items.map((item) => (
-              <ShoppingCartItem book={item} />
-            ))
-          }
-        </div>
-        <div className={styles.orderSummaryCard}>
-          <div className={styles.cardTitle}>Order Summary</div>
+      {
+        items.length > 0 ?
+          <div className={styles.orderContainer}>
+            <div className={styles.itemList}>
+              {
+                items &&
+                items.map((item) => (
+                  <ShoppingCartItem book={item} key={item.id} />
+                ))
+              }
+            </div>
+            <div className={styles.orderSummaryCard}>
+              <div className={styles.cardTitle}>Order Summary</div>
 
-          <div className={styles.numberOfProduct}>{3 > 1 ? 3 + " Products" : 3 + " Product"} </div>
+              <div className={styles.numberOfProduct}>{totalItems > 1 ? totalItems + " Products" : totalItems + " Product"} </div>
 
-          <div className={styles.amountContainer}>
-            <div className={styles.amountText}>Payment : </div>
-            <div className={styles.amount}>{15} TRY</div>
+              <div className={styles.amountContainer}>
+                <div className={styles.amountText}>Payment : </div>
+                <div className={styles.amount}>{totalPrice ? totalPrice + " TRY" : "FREE"}</div>
+              </div>
+
+              <Link to="/payment">
+                <button className={styles.buyButton}>
+                  Buy
+                </button>
+              </Link>
+            </div>
           </div>
+          :
+          <div className={styles.emptyCartContainer}>
+            <img
+              className={styles.cartImage}
+              src="/emptyCartImage.png"
+              alt="Empty Cart"
+            />
+            <div className={styles.emptyCartTitle}>Your Cart is Empty</div>
+            <div className={styles.emptyCartSubtitle}>Keep Shopping</div>
+            <Link to="/">
+              <button className={styles.homepageButton}>
+                Return to Homepage
+              </button>
+            </Link>
+          </div>
+      }
 
-          <button className={styles.buyButton}>
-            Buy
-          </button>
-
-        </div>
-      </div>
     </div>
   )
 }
